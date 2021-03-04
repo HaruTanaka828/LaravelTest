0a45e7edb8f71b8aa46c8307b7d827dffec95b3d
vagrant <vagrant@homestead>
2021-03-04 06:31:57 +0000

    add

---
+++ resources/views/test.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+  	<meta charset="utf-8">
+    <title>テストララベル</title>
+    <link rel="stylesheet" href="/css/styles.css">
+  </head>
+
+<body>
+
+	<div class="top">
+      <p class="menu">Menu</p>
+    <div class="top-text">
+        <p class="top-text2">シャンプー・カラー・トリートメント</p>
+        <p class="top-text3">上質な美容院にいつでも<span class="kayoi">通い放題</span></p>
+    </div>
+
+    <div class="mobile">
+    </div>
+
+<hr>
+
+</body>
+
+</html>
